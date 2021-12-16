a48116d6e111f65e8e5dbba08f4a79fce70eb69c
DEsimas <simasuh@gmail.com>
2021-12-16 17:07:12 +0300

    AccentTester: component added

--- src/Components/Accent-tester/AccentTester.js
+++ src/Components/Accent-tester/AccentTester.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import { useEffect, useState} from 'react';
+import crypto from 'crypto';
+
+import Loading from './../Loading/Loading';
+import Word from './Word';
+
+import loading from './../../Assets/AccentTester/loading.gif';
 
 import './AccentTester.scss';
 
+const SERVER_URL = 'http://localhost:3000/';
+
 export default function AccentTester() {
+    const [word, setWord] = useState(undefined);
+    const [counter, setCounter] = useState(0);
+
+    function decrypt(data) {
+        const key = data.content.slice(-32);
+        data.content = data.content.slice(0, -32);
+        const decipher = crypto.createDecipheriv('aes-256-ctr', key, Buffer(data.iv, 'hex'));
+        const decrpyted = Buffer.concat([decipher.update(Buffer.from(data.content, 'hex')), decipher.final()]);
+
+        return JSON.parse(decrpyted.toString());
+    }
+
+    useEffect(async() => {
+        const response = await fetch(SERVER_URL);
+        const data = await response.json();
+        const dictionary = decrypt(data);
+        setWord(new Word(dictionary, setCounter));
+    }, []);
+    
+    if(!word)  return <Loading loading={loading}/>
 
     return (
-        <div className="AccentTester">
-            <h1>AccentTester</h1>
+        <div className='AccentTester'>
+            <h1 className="AccentTester-Header"> Тест на знание ударений </h1>
+            {word.getJsx()}
+            <p className="AccentTester-Text">Основано на орфоэпическом словнике для ЕГЭ по Русскому языку 2022 год</p>
         </div>
-    )
+    );
 }
