89c2069d760255f647475d1f32a514e0cc539336
Serafim Sukharev <simasuh@gmail.com>
2022-06-25 12:12:16 +0300

    Previews fix

--- src/Components/Tests-hub/Tests.js
+++ src/Components/Tests-hub/Tests.js
@@ -1,30 +1,35 @@
+import AccentTester from "./../../Assets/Previews/AccentTester.png";
+import TypingTester from "./../../Assets/Previews/TypingTester.png";
+import ReactionTester from "./../../Assets/Previews/ReactionTester.png";
+import CameraTester from "./../../Assets/Previews/CameraTester.png";
+
 export default [
     {
         id: 1,
         name: "Accent tester",
         description: "Let's check your knowlage of russian accents",
         to: "/AccentTester/",
-        thumbnail: "https://cdn.discordapp.com/attachments/883231507349663754/917452664382566410/unknown.png"
+        thumbnail: AccentTester
     },
     {
         id: 2,
         name: "Typing tester",
         description: "Application for testing your typing speed. Supports custom texts",
         to: "/TypingTester/",
-        thumbnail: "https://cdn.discordapp.com/attachments/883231507349663754/917453615092867153/unknown.png"
+        thumbnail: TypingTester
     },
     {
         id: 3,
         name: "Camera tester",
         description: "Check your camera (or use it as a mirror)",
         to: "/CameraTester/",
-        thumbnail: "https://media.discordapp.net/attachments/883079967506833439/921065189837783060/2021-12-16_184236-min.png?width=1333&height=676"
+        thumbnail: CameraTester
     },
     {
         id: 4,
         name: "Reaction tester",
         description: "Reaction testing app ¯\\_(ツ)_/¯",
         to: "/ReactionTester/",
-        thumbnail: "https://cdn.discordapp.com/attachments/883231507349663754/917451465923129454/unknown.png"
+        thumbnail: ReactionTester
     }
 ]
