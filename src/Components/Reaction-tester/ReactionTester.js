150361342d20f8c92544dc70a7be46f6296b5be5
Serafim Sukharev <simasuh@gmail.com>
2022-06-27 14:47:29 +0300

    reaction tester clicked function

--- src/Components/Reaction-tester/ReactionTester.js
+++ src/Components/Reaction-tester/ReactionTester.js
@@ -24,12 +24,33 @@ export default function ReactionTester() {
         document.getElementsByClassName("check")[0].classList.add("red");
         begin = (new Date()).getMilliseconds();
     }
+
+    function clicked() {
+        if(status == "running") {
+            //clicked too early
+            status = "clicked";
+            showTooLateMsg();
+        } else if(status == "active") {
+            //clicked in time
+            status = "clicked";
+            end = (new Date()).getMilliseconds();
+            showStats();
+        }
+    }
+
+    function showStats() {
+        console.log("stats");
+    }
+
+    function showTooLateMsg() {
+        console.log("early");
+    }
     
     return (
         <div className="ReactionTester">
             <div className="ReactionTester-Buttons">
                 <div className="ReactionTester-Buttons-Button begin unselectable" onClick={start}> Press here to start </div>
-                <div className="ReactionTester-Buttons-Button check unselectable hidden" onClick={()=>{}}>Hit the button when the background turns red!</div>
+                <div className="ReactionTester-Buttons-Button check unselectable hidden" onClick={clicked}>Hit the button when the background turns red!</div>
             </div>
             <div className="ReactionTester-ResultWindow hidden">
                 <div className="ReactionTester-ResultWindow-InfoWindow">
